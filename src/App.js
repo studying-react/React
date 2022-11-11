840c591d5be7759c77172231598dcd7cf7c41d18
Patrik Hoferica <p.hoferica@outlook.com>
2022-11-11 11:08:32 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
-  //imperative approach
-  const para = document.createElement("p");
-  para.textContent = "Hello World!";
-  document.getElementById("root").append(para);
-  //
+  
 
   //props
   const expensesData = [
@@ -14,21 +10,26 @@ const App = () => {
       id: "e1",
       title: "Toilet paper",
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      dat: new Date(2021, 2, 28),
     },
     {
       id: "e2",
       title: "Car Insurance",
       amount: 394.67,
-      date: new Date(2021, 2, 28),
+      dat: new Date(2021, 2, 28),
     },
     {
       id: "e3",
       title: "shopping",
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      dat: new Date(2021, 2, 28),
     },
-    { id: "e4", title: "New TV", amount: 694.67, date: new Date(2021, 2, 28) },
+    { 
+      id: "e4", 
+      title: "New TV", 
+      amount: 694.67, 
+      dat: new Date(2021, 2, 28) 
+    }
   ];
 
   //
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,25 +9,25 @@ function Expenses (props) {
             <ExpenseItem
             title={props.expense[0].title} 
             amount={props.expense[0].amount}
-            date={props.expense[0].date}
+            date={props.expense[0].dat}
             />
 
             <ExpenseItem
             title={props.expense[1].title} 
             amount={props.expense[1].amount}
-            date={props.expense[1].date}
+            date={props.expense[1].dat}
             />
 
             <ExpenseItem
             amount={props.expense[2].amount}
             title={props.expense[2].title} 
-            date={props.expense[2].date}
+            date={props.expense[2].dat}
             />
 
             <ExpenseItem
             title={props.expense[3].title} 
             amount={props.expense[3].amount}
-            date={props.expense[3].date}
+            date={props.expense[3].dat}
             />
         </Card>
    
